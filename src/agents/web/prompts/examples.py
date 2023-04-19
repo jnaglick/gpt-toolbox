434452a2684e5d03785ea3dbedecd0f181612e45
jnaglick <jnaglick@gmail.com>
2023-04-19 18:08:01 -0400

    shorten examples

--- src/agents/web/prompts/examples.py
+++ src/agents/web/prompts/examples.py
@@ -42,7 +42,7 @@ def examples():
           user_context([[
             "WebSearch", 
             "current weather new york", 
-            "[('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New york, NY Weather Forecast and Conditions - The Weather Channel', 'https://weather.com/weather/today/l/New+York+NY+USNY0996:1:US'), ('Local Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/ny/new-york-weather'), ('Weather for New York, New York, USA - TimeAndDate', 'https://www.timeanddate.com/weather/usa/new-york'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city'), ('New York City, NY 10-Day Weather Forecast - The Weather Channel ...', 'https://weather.com/weather/tenday/l/96f2f84af9a5f5d452eb0574d4e4d8a840c71b05e22264ebdc0056433a642c84'), ('New York, New York 7 Day Weather Forecast - The Weather Network', 'https://www.theweathernetwork.com/us/weather/new-york/new-york'), ('New York, New York | Current Weather Forecasts, Live Radar Maps & News ...', 'https://www.weatherbug.com/weather-forecast/now/new-york-ny-10001'), ('NY Current Conditions - sfgate.com', 'https://www.sfgate.com/weather/article/ny-current-conditions-17896774.php'), ('NY Current Conditions', 'https://www.sfchronicle.com/weather/article/ny-current-conditions-17903155.php'), ('New York, New York, USA 14 day weather forecast - TimeAndDate', 'https://www.timeanddate.com/weather/usa/new-york/ext'), ('National Weather Service', 'https://forecast.weather.gov/MapClick.php?lat=40.71455000000003&lon=-74.00713999999994'), ('NWS Forecast Office New York, NY - National Weather Service', 'https://www.weather.gov/okx/'), ('New York - BBC Weather', 'https://www.bbc.com/weather/5128581'), ('National Weather Service', 'https://forecast.weather.gov/MapClick.php?zoneid=NYZ072'), ('New York - BBC Weather', 'https://www.bbc.com/weather/2641508'), ('New York (United States of America) weather - Met Office', 'https://www.metoffice.gov.uk/weather/forecast/dr5reg58f')]"
+            "[('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city')]"
           ]])
         ),
         "\n".join([
@@ -60,7 +60,7 @@ WebAccess: https://www.accuweather.com/en/us/new-york/10021/current-weather/3497
 Results: Access Denied
 |||||
 WebSearch: current weather new york
-Results: [('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New york, NY Weather Forecast and Conditions - The Weather Channel', 'https://weather.com/weather/today/l/New+York+NY+USNY0996:1:US'), ('Local Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/ny/new-york-weather'), ('Weather for New York, New York, USA - TimeAndDate', 'https://www.timeanddate.com/weather/usa/new-york'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city')]
+Results: [('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city')]
           """.strip()
         ),
         "\n".join([
@@ -81,7 +81,7 @@ WebAccess: https://www.accuweather.com/en/us/new-york/10021/current-weather/3497
 Results: Access Denied
 |||||
 WebSearch: current weather new york
-Results: [('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New york, NY Weather Forecast and Conditions - The Weather Channel', 'https://weather.com/weather/today/l/New+York+NY+USNY0996:1:US'), ('Local Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/ny/new-york-weather'), ('Weather for New York, New York, USA - TimeAndDate', 'https://www.timeanddate.com/weather/usa/new-york'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city')]
+Results: [('New York, NY Current Weather | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/current-weather/349727'), ('New York, NY Weather Forecast | AccuWeather', 'https://www.accuweather.com/en/us/new-york/10021/weather-forecast/349727'), ('New York City, NY Weather Conditions | Weather Underground', 'https://www.wunderground.com/weather/us/ny/new-york-city')]
           """.strip()
         ),
         example_answer("The current temperature in New York is 64 degrees Fahrenheit.")
